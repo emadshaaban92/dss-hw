ee63473ccad886dc9138b7156867ede87fcff0ce
Emad Shaaban <emad.shaaban92@gmail.com>
2012-12-22 02:42:27 +0200

    Handle Payoff Options

--- solver.py
+++ solver.py
@@ -3,6 +3,10 @@ import pandas as pd
 import sys
 
 infile = sys.argv[1]
+payoff = sys.argv[2]
+if payoff not in ("profit","cost") :
+    print payoff,"isn't a vaild payoff option, please use profit or cost only"
+    sys.exit()
 df = pd.read_csv(infile)
 if "Alternative" not in df.columns :
     print "Can't find Alternative Column in ", infile, ", Please use a valid input file."
@@ -18,17 +22,23 @@ m = int(raw_input())
 while m not in range(1,4) :
     print m , " is not valid,please choose a valid option"
     m = int(raw_input())
-
-func = min if m == 2 else max
+if payoff == "profit" :
+    func = min if m == 2 else max
+else :
+    func = min if m == 1 else max
 
 if m == 3:
-    for c in data.columns :
-        data[c] = data[c].max() - data[c]
+    if payoff == "profit" :
+        for c in data.columns :
+            data[c] = data[c].max() - data[c]
+    else :
+        for c in data.columns :
+            data[c] -= data[c].min() 
 
 data['choosen'] = pd.Series([func(o) for o in data.values],data.index)
 print data
 
-i = data['choosen'].argmin() if m == 3 else data['choosen'].argmax()
+i = data['choosen'].argmin() if m == 3 or payoff == "cost" else data['choosen'].argmax()
 
 print "\n\nBest Decision is " ,data.index[i] , " with value = " , data['choosen'][i] ,"\n\n"
 raw_input("Press any key to exit")
